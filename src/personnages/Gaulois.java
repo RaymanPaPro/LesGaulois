1ebd1372ca47bc7b9262fc5516fd62982f9c1b3a
Alex Bouslouf <RaymanPaPro@gmail.com>
2025-02-18 19:52:12 +0100

    deuxieme commit

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,13 +3,35 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
+	
 	public Gaulois(String nom, int force) {
 		super();
 		this.nom = nom;
 		this.force = force;
 	}
+	public String getNom() {
+		return nom;
+	}
 	
-	public 
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\""  );
+	}
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : "; 
+	}
 	
-
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Asterix",8);
+		System.out.println(asterix);
+		
+	}
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + "]";			
+		
+	}
+	private void frapper() {
+		
+		
+	}
 }
